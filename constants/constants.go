f7079891b651b37404e577e53b94edd6f1eec968
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2022-03-09 16:03:15 +0100

    Added some base urls for the apis the service is reliant on.

--- constants/constants.go
+++ constants/constants.go
@@ -13,12 +13,16 @@ const STATUS_LOCATION = "/corona/v1/status"
 const WEBHOOK_LOCATION = "/corona/v1/notifications/"
 const DEFAULT_LOCATION = "/"
 
-// API urls
+// Covid cases api
+const COVID_CASES_BASE_URL = "https://covid19-graphql.now.sh"
+
+// Covid tracker api
+const COVID_TRACKER_BASE_URL = "https://covidtrackerapi.bsg.ox.ac.uk/api"
 
 // URL params
 const URL_PARAM_AND = "&"
 const URL_PARAM_EQUALS = "="
 const URL_PARAM_LIMIT = "limit"
 
-// Web web_client timeout time
+// CLIENT_TIMEOUT web_client timeout time
 const CLIENT_TIMEOUT = 30 * time.Second
