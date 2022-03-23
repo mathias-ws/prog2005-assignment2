0ad3fdcce02eb4e1a1df62b148ecfb374aa5f99d
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2022-03-23 15:25:53 +0100

    Added call to initialize the db variables upon the start of the service.

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
-import "assignment-2/web_server"
+import (
+	"assignment-2/database"
+	"assignment-2/web_server"
+)
 
 // main starts the program.
 func main() {
+	database.InitDB()
 	web_server.StartWebServer()
 }
