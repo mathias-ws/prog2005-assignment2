f12012b3e1d52ed7ba43bd1b99da8378242f910f
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2022-03-23 16:15:56 +0100

    Added functions for deleting elements from the database.

--- database/controller.go
+++ database/controller.go
@@ -5,6 +5,7 @@ import (
 	"cloud.google.com/go/firestore"
 	"context"
 	firebase "firebase.google.com/go"
+	"google.golang.org/api/iterator"
 	"google.golang.org/api/option"
 	"log"
 )
@@ -59,4 +60,77 @@ func WriteToDatabase(collection string, document string, data interface{}) error
 	}
 
 	return nil
+}
+
+// DeleteDocument deletes a document from a collection.
+func DeleteDocument(collection string, document string) error {
+	client, errOpeningClient := app.Firestore(ctx)
+
+	if errOpeningClient != nil {
+		log.Println(errOpeningClient)
+		return custom_errors.GetDatabaseError()
+	}
+
+	_, err := client.Collection(collection).Doc(document).Delete(ctx)
+	if err != nil {
+		log.Printf("Error deleteing document: %v", err)
+		return err
+	}
+
+	errClosingClient := client.Close()
+
+	if errClosingClient != nil {
+		log.Println(errClosingClient)
+		return custom_errors.GetDatabaseError()
+	}
+
+	return nil
+}
+
+// DeleteCollection deletes all documents in a collection.
+func DeleteCollection(collection string) error {
+	client, errOpeningClient := app.Firestore(ctx)
+
+	if errOpeningClient != nil {
+		log.Println(errOpeningClient)
+		return custom_errors.GetDatabaseError()
+	}
+
+	// Code taken/inspired by the firestore documentation:
+	// https://firebase.google.com/docs/firestore/manage-data/delete-data
+	for {
+		// Get a batch of documents
+		iter := client.Collection(collection).Documents(ctx)
+		numDeleted := 0
+
+		// Iterate through the documents, adding
+		// a delete operation for each one to a
+		// WriteBatch.
+		batch := client.Batch()
+		for {
+			doc, err := iter.Next()
+			if err == iterator.Done {
+				break
+			}
+			if err != nil {
+				log.Printf("Error while interating all documents to delete: %v", err)
+				return err
+			}
+
+			batch.Delete(doc.Ref)
+			numDeleted++
+		}
+
+		// If there are no documents to delete,
+		// the process is over.
+		if numDeleted == 0 {
+			return nil
+		}
+
+		_, err := batch.Commit(ctx)
+		if err != nil {
+			log.Printf("Error commiting batch delete: %v", err)
+			return err
+		}
+	}
 }
