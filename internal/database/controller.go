f6e4350f7bb4089f139211fca25bf01701adf8a7
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2022-03-27 15:17:44 +0200

    Removed unused function.

--- internal/database/controller.go
+++ internal/database/controller.go
@@ -209,64 +209,6 @@ func DeleteDocument(collection string, document string) error {
 	return nil
 }
 
-// DeleteCollection deletes all documents in a collection.
-func DeleteCollection(collection string) error {
-	client, errOpeningClient := app.Firestore(ctx)
-
-	if errOpeningClient != nil {
-		log.Println(errOpeningClient)
-		return custom_errors.GetDatabaseError()
-	}
-
-	defer func() {
-		errClosingClient := client.Close()
-
-		if errClosingClient != nil {
-			log.Printf("Error closing db: %v", errClosingClient)
-		}
-	}()
-
-	hashedCollection, _ := hashing.HashString(collection)
-
-	// Code taken/inspired by the firestore documentation:
-	// https://firebase.google.com/docs/firestore/manage-data/delete-data
-	for {
-		// Get a batch of documents
-		iter := client.Collection(hashedCollection).Documents(ctx)
-		numDeleted := 0
-
-		// Iterate through the documents, adding
-		// a delete operation for each one to a
-		// WriteBatch.
-		batch := client.Batch()
-		for {
-			doc, err := iter.Next()
-			if err == iterator.Done {
-				break
-			}
-			if err != nil {
-				log.Printf("Error while interating all documents to delete: %v", err)
-				return err
-			}
-
-			batch.Delete(doc.Ref)
-			numDeleted++
-		}
-
-		// If there are no documents to delete,
-		// the process is over.
-		if numDeleted == 0 {
-			return nil
-		}
-
-		_, err := batch.Commit(ctx)
-		if err != nil {
-			log.Printf("Error commiting batch delete: %v", err)
-			return err
-		}
-	}
-}
-
 // IncrementCounter counts up the counter in one document.
 func IncrementCounter(collection string, document string) {
 	client, errClient := app.Firestore(ctx)
