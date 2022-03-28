173f0e4d024f7248ef174dc91042eb2eb199634c
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2022-03-28 09:22:11 +0200

    Split tests into multiple function.

--- internal/strutils/validation_test.go
+++ internal/strutils/validation_test.go
@@ -3,20 +3,50 @@ package strutils
 import "testing"
 import "github.com/stretchr/testify/assert"
 
-func TestCheckIfStringIsNotEmpty(t *testing.T) {
+func TestCheckIfStringIsNotEmptyValid(t *testing.T) {
 	assert.Equal(t, true, CheckIfStringIsNotEmpty("this is a cool string"))
+}
+
+func TestCheckIfStringIsNotEmptySpecialChar(t *testing.T) {
 	assert.Equal(t, false, CheckIfStringIsNotEmpty("this string will not work because of the !!"))
+}
+
+func TestCheckIfStringIsNotEmptyEmptyString(t *testing.T) {
 	assert.Equal(t, false, CheckIfStringIsNotEmpty(""))
+}
+
+func TestCheckIfStringIsNotEmptySpace(t *testing.T) {
 	assert.Equal(t, false, CheckIfStringIsNotEmpty(" "))
+}
+
+func TestCheckIfStringIsNotEmptySpaceAndNumber(t *testing.T) {
 	assert.Equal(t, false, CheckIfStringIsNotEmpty("number 2"))
+}
+
+func TestCheckIfStringIsNotEmptyCamelCase(t *testing.T) {
 	assert.Equal(t, true, CheckIfStringIsNotEmpty("superCoolString"))
 }
 
-func TestCheckIfValidDateFormat(t *testing.T) {
+func TestCheckIfValidDateFormatValid(t *testing.T) {
 	assert.Equal(t, true, CheckIfValidDateFormat("2022-12-22"))
+}
+
+func TestCheckIfValidDateFormatMissingDash(t *testing.T) {
 	assert.Equal(t, false, CheckIfValidDateFormat("202212-22"))
+}
+
+func TestCheckIfValidDateFormatValidDate(t *testing.T) {
 	assert.Equal(t, true, CheckIfValidDateFormat("2020-03-22"))
+}
+
+func TestCheckIfValidDateFormatZeroMonth(t *testing.T) {
 	assert.Equal(t, false, CheckIfValidDateFormat("2022-2-22"))
+}
+
+func TestCheckIfValidDateFormatZeroDate(t *testing.T) {
 	assert.Equal(t, false, CheckIfValidDateFormat("2022-12-2"))
+}
+
+func TestCheckIfValidDateFormatValidDate2(t *testing.T) {
 	assert.Equal(t, true, CheckIfValidDateFormat("2022-12-02"))
 }
