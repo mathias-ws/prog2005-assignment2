24e126c6f62fab138c4e779df4535c9f2563ac37
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2022-03-28 09:07:31 +0200

    Added tests for the GetUrlParametersPolicy function.

--- internal/web_server/urlutil/parameters_test.go
+++ internal/web_server/urlutil/parameters_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"net/url"
 	"testing"
+	"time"
 )
 
 func TestGetWebhookParameterNegative(t *testing.T) {
@@ -40,4 +41,52 @@ func TestGetWebhookParameterPositive(t *testing.T) {
 
 	assert.Contains(t, "a2c4d3e5e592a8b75da5d9b3a27ad846a40338ffe2ed00771179e63991619470", parameters1["id"])
 	assert.Nil(t, err1)
+}
+
+func TestGetUrlParametersPolicyNoParams(t *testing.T) {
+	testUrl := url.URL{RawQuery: ""}
+	parameters, err := GetUrlParametersPolicy(&testUrl)
+
+	assert.Empty(t, parameters)
+	assert.Equal(t, custom_errors.GetParameterError(), err)
+}
+
+func TestGetUrlParametersPolicyNoCountry(t *testing.T) {
+	testUrl := url.URL{RawQuery: "scope=2022-02-29"}
+	parameters, err := GetUrlParametersPolicy(&testUrl)
+
+	assert.Empty(t, parameters)
+	assert.Equal(t, custom_errors.GetParameterError(), err)
+}
+
+func TestGetUrlParametersPolicyInvalidDateFormat(t *testing.T) {
+	testUrl := url.URL{RawQuery: "country=Norway&scope=2022-2-10"}
+	parameters, err := GetUrlParametersPolicy(&testUrl)
+
+	assert.Equal(t, map[string]string(nil), parameters)
+	assert.Equal(t, custom_errors.GetParameterError(), err)
+}
+
+func TestGetUrlParametersPolicyValid(t *testing.T) {
+	testUrl := url.URL{RawQuery: "country=Norway&scope=2022-02-10"}
+	parameters, err := GetUrlParametersPolicy(&testUrl)
+
+	assert.Equal(t, map[string]string{
+		"country": "Norway",
+		"scope":   "2022-02-10",
+	}, parameters)
+
+	assert.Nil(t, err)
+}
+
+func TestGetUrlParametersPolicyWithoutScope(t *testing.T) {
+	testUrl := url.URL{RawQuery: "country=Norway"}
+	parameters, err := GetUrlParametersPolicy(&testUrl)
+
+	assert.Equal(t, map[string]string{
+		"country": "Norway",
+		"scope":   time.Now().Format("2006-01-02"),
+	}, parameters)
+
+	assert.Nil(t, err)
 }
