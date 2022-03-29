448fc41e6960620a06c3863245ee9d3cd33cf56c
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2022-03-29 15:11:41 +0200

    Added ability to set vars in test mode.

--- internal/buisness_logic/country/constants.go
+++ internal/buisness_logic/country/constants.go
@@ -1,11 +1,17 @@
 package country
 
-var countryApiUrl = "https://restcountries.com/v3.1/alpha/"
+var (
+	countryApiUrl       = "https://restcountries.com/v3.1/alpha/"
+	countryDbCollection = "cca3tocountry"
+)
 
 const countryApiCodeParam string = "?codes="
 const countryApiFieldParam string = "&fields=name"
 
-const countryDbCollection string = "cca3tocountry"
+// SetTestCollection sets the test collection.
+func SetTestCollection() {
+	countryDbCollection = "testcca3tocountry"
+}
 
 // SetTestUrlCountry sets the test url for the country api.
 func SetTestUrlCountry(testUrl string) {
