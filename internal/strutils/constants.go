afb0a950748621c654a4389bf09014009f4eb3a9
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2022-04-07 14:33:57 +0200

    Changed regex to only accept dates from the year 2019-202x.

--- internal/strutils/constants.go
+++ internal/strutils/constants.go
@@ -2,7 +2,7 @@ package strutils
 
 // RegexCheckValidDate regex expression for checking if string is valid date format.
 // Inspired by: https://www.golangprograms.com/regular-expression-to-validate-the-date-format-in-dd-mm-yyyy.html
-const regexCheckValidDate string = "((19|20)\\d\\d)-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])"
+const regexCheckValidDate string = "(2019|202\\d)-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])"
 
 // RegexCheckValidString regex expression that checks that a string contains small or capital letters.
 const regexCheckValidString string = "^[a-zA-Z]+$"
